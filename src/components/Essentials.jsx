3e2c2f0a676e122068d86cd5b8cb6516cd603ac2
Stainzxc <106868902+Stainzxc@users.noreply.github.com>
2023-05-17 21:41:09 +0800

    responsive

--- src/components/Essentials.jsx
+++ src/components/Essentials.jsx
@@ -4,16 +4,16 @@ import image2 from "../assets/images/image2.jpg";
 
 export default function Essentials() {
   return (
-      <div className="max-w-[1240px] mx-auto my-10 h-screen text-black bg-white">
+      <div className="max-w-[1240px] mx-auto my-10  text-black bg-white">
         <div className="grid md:grid-cols-2 sm:grid-cols-2">
           <div className="relative">
-            <img src={image1} alt="image1" className="h-screen w-full"/>
+            <img src={image1} alt="image1" className="h-auto w-full"/>
             <div className="absolute bottom-0 left-0 mb-10 ml-10 rounded-full bg-white px-6 py-2 text-lg">
               <button>Men's</button>
             </div>
           </div>
           <div className="relative">
-            <img src={image2} alt="image2" className="h-screen w-full"/>
+            <img src={image2} alt="image2" className="h-auto w-full"/>
             <div className="absolute bottom-0 left-0 mb-10 ml-10 rounded-full bg-white px-6 py-2 text-lg">
                 <button>
                     Women's
