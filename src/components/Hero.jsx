e0de5d4a137cebf738761fef91e838adb39467d2
Stainzxc <106868902+Stainzxc@users.noreply.github.com>
2023-05-17 21:32:48 +0800

    independencies

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Typed from 'react-typed';
+// import Typed from 'react-typed';
 
 export default function Hero() {
   return (
@@ -8,8 +8,8 @@ export default function Hero() {
       <p className="text-[#ff7d1a] md:text-4xl text-3xl font-bold p-2">Sneaker Company</p>
       <h1 className="md:text-7xl sm:text-6xl text-4xl font-bold md:py-4">Limited Edition Sneakers</h1>
       <div className="flex justify-center items-center">
-      <p className="md:text-5xl sm:text-4xl text-2xl font-bold py-2">Best </p>
-      <Typed className="md:text-5xl sm:text-4xl text-2xl font-bold md:pl-4 pl-2 text-[#ff7d1a]" strings ={["Shoes", "Style"]} typeSpeed={120} backSpeed={140} loop></Typed>
+      <p className="md:text-5xl sm:text-4xl text-2xl font-bold py-2">Best <span className="text-[#ff7d1a]">Style</span></p>
+      
       </div>
       <p className="text-slate-700 font-bold py-2 md:text-2xl text-xl ">Shoes are boring. Wear sneakers.</p>
       <button className="bg-[#ff7d1a] w-[200px] rounded-md my-4 py-4 mx-auto text-white font-bold">Get Started</button>
